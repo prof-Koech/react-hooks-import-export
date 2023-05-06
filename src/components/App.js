010a48d9f8642edbcebaf21dce9fa79284ffca8b
Kennedy <kiplangatkennedy019@gmail.com>
2023-05-06 13:03:39 +0300

    done

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from './Home'
+import NavBar from "./NavBar";
+import About from "./About";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
